04ec8129d938b7323ad425018170a20fbed6d964
Hussar99 <43067400+Hussar99@users.noreply.github.com>
2018-09-21 14:17:31 +0100

    Adding Critter and Score

---
+++ Assets/Scripts/Critter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Critter : MonoBehaviour {
+
+    public Vector2 lowerRange;
+    public Vector2 upperRange;
+
+    public Score scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
+            Random.Range(lowerRange.y, upperRange.y),
+            0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        scoreDisplay.ChangeValue(1);
+
+        Destroy(gameObject);
+    }
+}
